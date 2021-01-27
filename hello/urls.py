20f48bcf834fa52270404ad3743876a45b3c1290
Alan Jui <alanjui.1960@gmail.com>
2021-01-27 22:47:54 +0800

    加入 Layout Template 應用。

--- hello/urls.py
+++ hello/urls.py
@@ -4,5 +4,7 @@ from hello import views
 
 urlpatterns = [
     path("hello/<name>/", views.hello_there, name="hello_there"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
     path("", views.home, name="home"),
 ]
