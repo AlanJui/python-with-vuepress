21e3b3704411b720317fc2c9021dfbf935ac0350
Alan Jui <alanjui.1960@gmail.com>
2021-01-28 20:57:50 +0800

    完成實作

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,37 @@
 import re
 from datetime import datetime
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.shortcuts import render, redirect
 
+from hello.models import LogMessage
+from hello.forms import LogMessageForm
+
+class LogMessageListView(ListView):
+    model = LogMessage
+    template_name = "hello/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(LogMessageListView, self).get_context_data(**kwargs)
+        return context
+
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect('home')
+    else:
+        return render(
+            request,
+            'hello/log_message.html',
+            {
+                "form": form
+            }
+        )
 
 def home(request):
     # return HttpResponse("Hello, Django!")
